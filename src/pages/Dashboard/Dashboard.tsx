95e4f92d3c56ec13c2fe826169908222a74aceba
shmy <pandahao123>
2020-03-17 16:08:10 +0800

    feat(build): add unit test

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Avatar, Dropdown, Layout, Menu} from "antd";
+import { LogoutOutlined } from '@ant-design/icons';
+import styles from "./Dashboard.module.scss";
+import history from "@/utils/history";
 
 const Dashboard: React.FC = props => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <div>dashboard</div>
+    <Layout className={styles.dashboard}>
+      <Layout.Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+        <Menu theme="dark" mode="inline">
+        </Menu>
+      </Layout.Sider>
+      <Layout>
+        <Layout.Header className={styles.header}>
+          <div className={styles.headerContent} />
+          <Dropdown overlay={
+            <Menu>
+              <Menu.Item onClick={() => {
+                history.replace('/login');
+              }}><LogoutOutlined />退出登录</Menu.Item>
+            </Menu>
+          } trigger={['hover']}>
+            <Avatar style={{backgroundColor: '#87d068'}}>U</Avatar>
+          </Dropdown>
+        </Layout.Header>
+        <Layout.Content>Content</Layout.Content>
+      </Layout>
+    </Layout>
   );
 };
 
--- src/pages/Dashboard/__tests__/Dashboard.spec.tsx
+++ src/pages/Dashboard/__tests__/Dashboard.spec.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {mount} from "enzyme";
 import Dashboard from "@/pages/Dashboard/Dashboard";
+import {Dropdown, Menu} from "antd";
+import history from "@/utils/history";
+jest.mock('@/utils/history', () => {
+  return {
+    replace: jest.fn(),
+  };
+});
 
 describe('Test Dashboard', () => {
   it('should render without crashing', () => {
-    mount(<Dashboard />);
+
+    const wrapper = mount(<Dashboard />);
+    const overlayWrapper = mount(<>{wrapper.find(Dropdown).prop('overlay')}</>);
+    overlayWrapper.find(Menu.Item).simulate('click');
+    expect(history.replace).toBeCalledWith('/login');
   });
 });
