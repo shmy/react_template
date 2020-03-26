7fb50993858a699cfdf247becf621ab15d77e702
shmy <pandahao123>
2020-03-26 11:16:15 +0800

    test ci

--- build-development.sh
+++ build-development.sh
@@ -4,4 +4,5 @@ cd deploy
 docker build -t shmy/sso-web .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker push shmy/sso-web:latest
-# sudo apt-get update && sudo apt-get install sshpass -y
+sudo apt-get update && sudo apt-get install sshpass -y
+sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no root@47.75.55.94 "cd /home/sso && ./start_web.sh"
