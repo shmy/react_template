cc83e662607ff237e63e31481dfcd3e974f91dc7
shmy <pandahao123>
2020-03-28 18:48:34 +0800

    sso退出登录后 子系统也应该退出登录

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import {logout} from "@/utils/history";
 import {LogoutOutlined} from '@ant-design/icons';
 import {useDashboardContext} from "@/pages/Dashboard/Dashboard";
 import {SERVER_STATIC_PATH} from "@/components/SingleImageUpload/SingleImageUpload";
+import http from "@/utils/http";
 
 interface HeaderProps {
 
@@ -13,7 +14,7 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = props => {
   const {user} = useDashboardContext();
   const handleLogout = () => {
-    logout();
+    http.delete('/logout').then(logout).catch(logout);
   };
   return (
     <Layout.Header className={styles.header}>
@@ -23,7 +24,8 @@ const Header: FC<HeaderProps> = props => {
           <Menu.Item onClick={handleLogout}><LogoutOutlined/>退出登录</Menu.Item>
         </Menu>
       } trigger={['hover']}>
-        <Avatar src={SERVER_STATIC_PATH + (user as any).avatar_url} style={{backgroundColor: '#00aaff'}}>{(user as any).real_name || (user as any).username}</Avatar>
+        <Avatar src={SERVER_STATIC_PATH + (user as any).avatar_url}
+                style={{backgroundColor: '#00aaff'}}>{(user as any).real_name || (user as any).username}</Avatar>
       </Dropdown>
     </Layout.Header>
   );
